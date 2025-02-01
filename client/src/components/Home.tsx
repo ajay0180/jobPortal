4e7e84617288839c303bba03f7e9f8a4b6757a5b
Ajay Kumar <jangraajay489@gmail.com>
2025-02-01 13:26:27 +0530

    Delete client directory

--- client/src/components/Home.tsx
+++
@@ -1,9 +0,0 @@
-import NavBar from "./shared/NavBar";
-
-export const Home = () => {
-  return (
-    <div>
-      <NavBar />
-    </div>
-  );
-};
--- client/src/utils/constant.ts
+++
@@ -1,1 +0,0 @@
-export const USER_API_ENDPOINT: string = "http://localhost:8080/api/v1/user";
