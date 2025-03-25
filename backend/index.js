a4910e791f1779a9c0c6f11f92c31172fafefce0
ajay0180 <jangraajay489@gmail.com>
2025-03-26 04:58:15 +0530

    updation3

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["*"],
+    origin: ["http://localhost:5173"],
     allowedHeaders: [
       "Content-Type",
       "Authorization",
